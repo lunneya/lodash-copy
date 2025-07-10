127bd52af027c0c07957c7480054a9d166d92344
beeeeaaa <emmi.ovechkina@gmail.com>
2025-07-10 21:43:21 +0500

    rewrote and supplemented slice

--- src/array/slice/slice.ts
+++ src/array/slice/slice.ts
@@ -1,11 +1,27 @@
-function _slice(array: any[], start: number, end: number): Array<any> {
+function _slice(array: any[], start?: number, end?: number): Array<any> {
     let result: Array<any> = [];
-    array.forEach((item: any, index: any) => {
-        if (index + 1 >= start && index + 1 <= end) {
-            result.push(item);
+
+    // Если не переданы start && end
+    if (start === undefined && end === undefined) return array;
+
+
+    array.forEach((item: any, index: number) => {
+        // Если переданы start && end
+        if (start !== undefined && end !== undefined) {
+            if (index >= start && index < end) {
+                result.push(item);
+            }
+        }
+
+        // Если передан только start
+        if (start !== undefined && end === undefined) {
+            if (index >= start) {
+                result.push(item);
+            }
         }
     })
     return result;
 }
 
+console.log(_slice([1,2,3,4,5,6,7,8], 2))
 export default _slice;
